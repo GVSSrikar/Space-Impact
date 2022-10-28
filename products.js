452d8f9c5e71b0c4eb956fe0dc15946d63df99f2
GVSSrikar <53863991+GVSSrikar@users.noreply.github.com>
2022-10-28 23:33:47 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    id: 0,
+    name: "Life Pack",
+    price: 500,
+    instock: 100,
+    description:
+      "increase health of player by 10 ",
+    imgSrc: "./img/health-boost.png",
+  },
+  {
+    id: 1,
+    name: "Ship Mach Techno II",
+    price: 600,
+    instock: 43,
+    description:
+      "Changing your ship",
+    imgSrc: "./img/spaceship2.jpg",
+  },
+  {
+    id: 2,
+    name: "T-shirt 3",
+    price: 300,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t3.png",
+  },
+  {
+    id: 3,
+    name: "T-shirt 4",
+    price: 800,
+    instock: 5,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t4.png",
+  },
+  {
+    id: 4,
+    name: "T-shirt 5",
+    price: 1000,
+    instock: 4,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t5.png",
+  },
+  {
+    id: 5,
+    name: "T-shirt 6",
+    price: 1200,
+    instock: 40,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t6.png",
+  },
+];
